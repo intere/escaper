a0a01e50b072d341d72cd1decd4125a0e9a4a542
Eric Internicola <einternicola@silverchalice.com>
2014-02-02 08:42:16 -0700

    Updated the app so that it is more useful (reads from / writes to files, supports various output formats, etc), also provided some nice Usage info.

--- README.txt
+++ README.txt
@@ -14,4 +14,26 @@ Running:
     mvn clean install exec:java -Dexec.args="/tmp/test.xml" 
 
 
+Usage:
+====
 
+    escaper <output_type> <input_file> [output_file]
+
+    Parameters:
+      output_type - The desired output type (what to escape the input file to).  You can use any one of the following:
+        -java
+          Produces a Java String
+        -javascript
+          Produces a Javascript String
+        -sql
+          Produces a SQL String
+        -xml
+          Produces an XML String
+        -html
+          Produces an HTML String
+
+      input_file - The Path (relative or absolute) to the input file you would like to escape
+
+      output_file - The (optional) path (relative or absolute) to the output file you would like to write the string to.  If you don't provide this, it will just be written to STDOUT.
+
+    
--- src/main/java/com/colasoft/App.java
+++ src/main/java/com/colasoft/App.java
@@ -1,48 +1,141 @@
 package com.colasoft;
 
-/**
- * Hello world!
- *
- */
 import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.commons.io.FileUtils;
 import java.io.File;
 
+/**
+ * Tool that uses Apache Commons Lang to escape strings for you into various output formats.
+ */
 public class App {
 
   public static void main(String args[]) throws Exception {
 
-  	if(args.length < 1) {
-  		System.out.println("Error, you must provide an XML file path to convert");
-  		return;
-  	}
+    Params params = parseCLI(args);
 
-  	File file = new File(args[0]);
-
+    // First, open the file:
+    File file = new File(params.inputFile);
   	String fileData = FileUtils.readFileToString(file, null);
-  	System.out.println(StringEscapeUtils.escapeJavaScript(fileData));
 
+    // Now - go get the appropriate output format:
+    String output = null;
 
+    switch(params.outputType) {
+      case JavaString: {
+        output = "String data=\"" + StringEscapeUtils.escapeJava(fileData) + "\";";
+        break;
+      }
 
-    // String unescapedJava = "Are you for real?";
-    // System.err.println(
-    //   StringEscapeUtils.escapeJava(unescapedJava));
+      case JavaScriptString: {
+        output = "var data='" + StringEscapeUtils.escapeJavaScript(fileData) + "';";
+        break;
+      }
 
-    // String unescapedJavaScript = "What's in a name?";
-    // System.err.println(
-    //   StringEscapeUtils.escapeJavaScript(unescapedJavaScript));
+      case SqlString: {
+        output = StringEscapeUtils.escapeSql(fileData);
+        break;
+      }
 
-    // String unescapedSql = "Mc'Williams";
-    // System.err.println(
-    //   StringEscapeUtils.escapeSql(unescapedSql));
+      case XmlString: {
+        output = StringEscapeUtils.escapeXml(fileData);
+        break;
+      }
 
-    // String unescapedXML = "<data>";
-    // System.err.println(
-    //   StringEscapeUtils.escapeXml(unescapedXML));
+      case HtmlString: {
+        output = StringEscapeUtils.escapeHtml(fileData);
+        break;
+      }
 
-    // String unescapedHTML = "<data>";
-    // System.err.println(
-    //   StringEscapeUtils.escapeHtml(unescapedHTML));
+      default: {
+        System.err.println("Error, unknown escape type");
+      }
+    }
 
+    if(null != params.outputFile) {
+      FileUtils.writeStringToFile(new File(params.outputFile), output, null);
+    } else {
+  	 System.out.println(output);
+    }
   }
+
+  /** Enum to keep track of output types.  */
+  private enum OutputType {
+    JavaString("-java"),
+    JavaScriptString("-javascript"),
+    SqlString("-sql"),
+    XmlString("-xml"),
+    HtmlString("-html");
+
+    private String arg;
+
+    public String getArg() {
+      return arg;
+    }
+
+    OutputType(String arg) {
+      this.arg = arg;
+    }
+
+    static OutputType getOutputType(String arg) {
+      for(OutputType type : OutputType.values()) {
+        if(type.getArg().equals(arg)) {
+          return type;
+        }
+      }
+      return null;
+    }
+  }
+
+  /** Private "struct" to keep track of CLI parameters.  */
+  private static class Params {
+    private String inputFile;
+    private String outputFile;
+    private OutputType outputType;
+  }
+
+  /** Routine to parse CLI arguments.  */
+  private static Params parseCLI(String[] args) {
+    Params params = new Params();
+
+    if(args.length>1) {
+      int paramIndex = 0;
+      for(int i=0; i<args.length; i++) {
+        String arg = args[i];
+        OutputType type = OutputType.getOutputType(arg);
+        if(type!=null) {
+          params.outputType = type;
+          paramIndex = i;
+          break;
+        } else if("-help".equals(arg)) {
+          usage();
+          System.exit(0);
+        }
+      }
+
+      params.inputFile = args[paramIndex+1];
+      if(args.length>paramIndex+2) {
+        params.outputFile = args[paramIndex+2];
+      }
+
+    } else if(args.length == 1 && "-help".equals(args[0])) {
+      usage();
+      System.exit(0);
+
+    } else {
+      
+      System.err.println("Error, incorrect usage");
+      usage();
+      System.exit(-1);
+
+    }
+
+    return params;
+  }
+
+  /** Produces Usage Message.  */
+  private static void usage() {
+    String usage="Usage:\n\tescaper <output_type> <input_file> [output_file]\n\nParameters:\n\toutput_type - The desired output type (what to escape the input file to).  You can use any one of the following:\n\t\t-java \n\t\t\tProduces a Java String\n\t\t-javascript\n\t\t\tProduces a Javascript String\n\t\t-sql\n\t\t\tProduces a SQL String\n\t\t-xml\n\t\t\tProduces an XML String\n\t\t-html\n\t\t\tProduces an HTML String\n\n\tinput_file - The Path (relative or absolute) to the input file you would like to escape\n\n\toutput_file - The (optional) path (relative or absolute) to the output file you would like to write the string to.  If you don't provide this, it will just be written to STDOUT.\n\n";
+    System.out.println(usage);
+  }
+
 }
